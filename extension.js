bb02665ea72d2f54a6c20db944fe787fde46225a
Shiro OSL <213114305+ShiroOSL@users.noreply.github.com>
2026-07-02 13:18:10 +0100

    Add settings initialization and cleanup
    Initialize settings in the constructor and clean up in disable method.

--- extension.js
+++ extension.js
@@ -5,10 +5,15 @@ export default class GlobalMenuExtension extends Extension {
     constructor(metadata) {
         super(metadata);
         this._menuManager = null;
+        this._settings = null;
     }
 
     enable() {
         console.log(`[globalmenu@ShiroOSL.github.io] Enabling extension.`);
+        
+        // Pass the GNOME-mandated base schema ID string
+        this._settings = this.getSettings('org.gnome.shell.extensions.globalmenu');
+        
         // Pass the extension instance context down to the manager
         this._menuManager = new MenuManager(this);
     }
@@ -18,6 +23,10 @@ export default class GlobalMenuExtension extends Extension {
         if (this._menuManager) {
             this._menuManager.destroy();
             this._menuManager = null;
+        }
+        
+        if (this._settings) {
+            this._settings = null;
         }
     }
 }
