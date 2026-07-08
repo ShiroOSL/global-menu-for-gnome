7e74c6c0f594fd4da1d74668b19fb665140e9670
Shiro OSL <213114305+ShiroOSL@users.noreply.github.com>
2026-07-08 23:22:24 +0100

    Refactor focus window connection handling

--- extension.js
+++ extension.js
@@ -6,7 +6,6 @@ export default class GlobalMenuExtension extends Extension {
         super(metadata);
         this._menuManager = null;
         this._settings = null;
-        this._focusNotifyId = 0;
     }
 
     enable() {
@@ -14,27 +13,22 @@ export default class GlobalMenuExtension extends Extension {
         
         this._settings = this.getSettings('org.gnome.shell.extensions.globalmenu');
         
-        // Correctly pass the string UUID from metadata
         const uuid = this.metadata.uuid || 'globalmenu@ShiroOSL.github.io';
         this._menuManager = new MenuManager(uuid);
 
-        // Track active window adjustments dynamically
         let initialWindow = global.display.get_focus_window();
         this._menuManager.updateMenuForWindow(initialWindow);
 
-        this._focusNotifyId = global.display.connect('notify::focus-window', () => {
+        global.display.connectObject('notify::focus-window', () => {
             let activeWindow = global.display.get_focus_window();
             this._menuManager.updateMenuForWindow(activeWindow);
-        });
+        }, this);
     }
 
     disable() {
         console.log(`[globalmenu@ShiroOSL.github.io] Disabling extension.`);
         
-        if (this._focusNotifyId > 0) {
-            global.display.disconnect(this._focusNotifyId);
-            this._focusNotifyId = 0;
-        }
+        global.display.disconnectObject(this);
 
         if (this._menuManager) {
             this._menuManager.destroy();
